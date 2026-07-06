ab33470550245b460a92966aa777520414776778
Election Surveillance Dev <dev@election-surveillance.local>
2026-07-06 02:23:31 -0700

    To point fronend to backed on AWS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://15.207.113.239:5000',
         changeOrigin: true,
       },
     },
